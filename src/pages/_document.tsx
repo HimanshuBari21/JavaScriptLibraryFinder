2a3580dcc3a2b61c3fe19f06db58484f86dc863d
Himanshu Bari <himanshubari789@gmail.com>
2024-12-09 03:49:01 +0530

    Added some META details

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,21 @@ const Document = () => {
   return (
     <Html lang="en">
       <Head>
+        <meta name="title" content="JavaScriptLibraryFinder" />
+        <meta
+          name="description"
+          content="There is a JS library for that - Find JS library for tasks"
+        />
+        <meta
+          name="keywords"
+          content="jstools, javascript, devtools,javascrio tools"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="2 days" />
+        <meta name="author" content="Himanshu Bari" />
+
         <ColorSchemeScript defaultColorScheme="auto" />
       </Head>
       <body className="antialiased">
