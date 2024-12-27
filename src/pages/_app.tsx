5d2f06adcb1a6618411d2301e63f2eb611004c03
Himanshu Bari <himanshubari789@gmail.com>
2024-12-27 18:54:39 +0530

    Add Vercel Speed Insights integration to improve performance tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import { NavigationProgress } from "@mantine/nprogress";
 import AppShellLayout from "@/components/AppShellLayout";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <AppShellLayout>
         <NavigationProgress />
         <Component {...pageProps} />
+        <SpeedInsights />
       </AppShellLayout>
     </MantineProvider>
   );
